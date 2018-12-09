c711f3a69904510c91893137aa9dd142c1c0317e
Chey <32723997+suncheyn@users.noreply.github.com>
2018-12-08 20:12:51 -0800

    Fully implemented Firebase, added Topics.java and Flashcard.java

--- app/src/main/java/com/example/cheyenne/flashcardapp/addTopic.java
+++ app/src/main/java/com/example/cheyenne/flashcardapp/addTopic.java
@@ -5,19 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class addTopic extends AppCompatActivity {
     private Button button;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_topic);
 
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Topics");
+
         button = findViewById(R.id.topicFinish);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String title = "ICS 45J";
+                String key = myRef.push().getKey();
+                myRef.child(key).setValue(title);
                 openAddCard();
             }
         });
@@ -27,4 +41,5 @@ public class addTopic extends AppCompatActivity {
         Intent intent = new Intent(this, addCard.class);
         startActivity(intent);
     }
+
 }
